9f2f8632209de2bd019f0704a831d5eb5452e4ba
Bhavik_Prajapati_7055 <109065176+prajapatibhavik2702@users.noreply.github.com>
2025-05-09 14:20:47 +0530

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    $fourthHighestSalary = DB::table('salaries')
+    ->select('amount')
+    ->distinct() // Ensure only unique salary amounts are considered
+    ->orderByDesc('amount') // Highest first
+    ->skip(3) // Skip top 3 unique salaries
+    ->take(1) // Take the 4th unique salary
+    ->first();
 }
